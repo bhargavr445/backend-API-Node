f6ec2bc134a7f0698e8218198c7ffe7e3e90e6de
bhargavr445 <bhargavr445@gmail.com>
2020-09-07 23:54:42 -0400

    code clean up

--- src/routes/student.js
+++ src/routes/student.js
@@ -19,10 +19,8 @@ router.get('/api/student/:id', auth, (req, res) => {
 //this is return all students
 router.get('/api/student', auth, (req, res) => {
     Student.find({}).then((result) => {
-        // console.log(result);
         return res.status(200).send({ data: result, status: 1 });
     }).catch((err) => {
-        // console.log(err);
         return res.status(400).json({ error: error, status: 0 })
     });
 });
@@ -39,25 +37,20 @@ router.get('/api/getAllIds', auth, (req, res) => {
 //this will create student.
 router.post('/api/student', auth, (req, res) => {
     const student = new Student(req.body);
-    //console.log(student);
     student.save().then((resp) => {
         let resp1 = new Student();
-        //console.log(resp1);
         resp1 = resp;
         return res.status(200).send({ data: resp1.studentId, status: 1 });
     }).catch((error) => {
-        // console.log(error);
         return res.status(500).send({ data: error.message, status: 0 });
     });
 });
 
 router.put('/api/student', auth, (req, res) => {
-    console.log(req.body);
     const stu = new Student(req.body);
     Student.findByIdAndUpdate(stu.studentId, stu).then(resp => {
         return res.status(200).send({ data: resp, status: 1 });
     }).catch((err) => {
-        console.log(err);
         return res.status(500).send({ data: err, status: 0 });
     });
 });
