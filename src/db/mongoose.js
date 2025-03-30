2f89be2a76f8d5d859af448ffe4e6828af7d24dc
Bhargav R G <bhargavr445@gmail.com>
2025-03-30 01:42:18 -0400

    updated url

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,16 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const userName = 'bhargav';
-const password = 'Wednesday40';
-const clusterInfo = 'cluster0.xoqibqx.mongodb.net'
-
-const atlasMongoConnection = mongoose.createConnection(`mongodb+srv://${userName}:${password}@${clusterInfo}/task-manager-api`, {
+const atlasMongoConnection = mongoose.createConnection(`mongodb+srv://${process.env.ATLAS_USER_NAME}:${process.env.ATLAS_PSWD}@${process.env.CLUSTER_INFO}/task-manager-api`, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
 })
 
-const localDSchemas = mongoose.createConnection('mongodb://127.0.0.1:27017/task-manager-api', {
+const localDSchemas = mongoose.createConnection(`mongodb+srv://${process.env.ATLAS_USER_NAME}:${process.env.ATLAS_PSWD}@${process.env.CLUSTER_INFO}/task-manager-api`, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
