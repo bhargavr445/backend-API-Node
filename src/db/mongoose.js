490766fa7e24ad5c8c8839d5830d8d172f7d5a20
Bhargav R G <bhargavr445@gmail.com>
2025-03-30 20:34:33 -0400

    updated mongo url

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const atlasMongoConnection = mongoose.createConnection(`mongodb+srv://${process.env.ATLAS_USER_NAME}:${process.env.ATLAS_PSWD}@${process.env.CLUSTER_INFO}/task-manager-api`, {
+
+const atlasMongoConnection = mongoose.createConnection(process.env.MONGO_DB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
